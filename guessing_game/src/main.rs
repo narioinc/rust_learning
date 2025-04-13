e01dd2a4b917fefd53f97e517516319293c49cee
narioinc89 <narioinc89@gmail.com>
2025-04-13 18:10:41 +0530

    chapter 2 complete

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,47 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+    println!("guess the number !");
+   
+
+    let secret_number = rand::rng().random_range(1..100);
+    println!("The secret number is: {}", secret_number);
+
+    loop{
+    println!("Please input your guess.");
+    
+    
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    //expect allows you to catch the Err object from Result
+    //let guess: u32 = guess.trim().parse().expect("please type a number !");
+
+    //Here, we can use the OK and Err from Result to create alternate code paths
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please type a number!");
+            continue;
+        },
+    };
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }    
+}
+    //println!("You guessed: {}", guess);
+
+    
+}
